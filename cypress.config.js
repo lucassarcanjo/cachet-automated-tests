fc65e0a2830da09eee20aa26ccf45e222fd6f5ba
Lucas Coelho <lucasfernandocoelho15@gmail.com>
2023-12-04 10:42:02 -0300

    Move Cypress tests to new folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "tests/e2e/cypress/e2e/*.cy.js",
+    supportFile: false,
   },
-});
+};
